76c0102de1460cc5306b2bd6b674011d155ade04
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-08 10:55:13 -0700

    Python Script

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word[:3]
+word[-2:]
+word[3:-2]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}"
