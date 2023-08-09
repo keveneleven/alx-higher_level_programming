2e7b345f6daa0661efbfc88d8f3dad913dc2ee58
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-09 13:06:17 -0700

    if else loops and functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ i = 0
 c = ord('z')
 
 while c >= ord('a'):
-    print(chr(c - (32 if (c - ord('a')) % 2 == 0 else 0)), end="")
+    print("{}".format(chr(c - (32 if (c - ord('a')) % 2 == 0 else 0))), end="")
     c -= 1
