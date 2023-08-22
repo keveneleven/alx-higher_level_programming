dc521cae38234a644897611052ceb94b4038b571
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-22 08:42:42 -0700

    Python Import Modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,11 +4,9 @@ def magic_calculation(a, b):
 
     if a < b:
         c = add(a, b)
-        i = 4
-        while i < 6:
+        for i in range(4, 6):
             c = add(c, i)
-            i += 1
-        return c
-    else:
-        return sub(a, b)
+        return (c)
 
+    else:
+        return(sub(a, b))
