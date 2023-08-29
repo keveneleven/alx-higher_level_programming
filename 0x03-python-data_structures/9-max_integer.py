245d22d60d7cd116f8b41e805201fb1d96c786ea
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-29 10:43:58 -0700

    Python Data Structures

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    biggest = my_list[0]
+    i = 0
+    while i < len(my_list):
+        if my_list[i] > biggest:
+            biggest = my_list[i]
+        i += 1
+
+    return biggest
+
