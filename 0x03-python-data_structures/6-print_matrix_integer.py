e2f773b5b5b47ea8526501b7b72012b20513eb4e
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-29 10:29:14 -0700

    Python Data Structure

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            print("{:d}".format(matrix[i][j]), end=" " if j != len(matrix[i]) - 1 else "")
+            j += 1
+        print()
+        i += 1
