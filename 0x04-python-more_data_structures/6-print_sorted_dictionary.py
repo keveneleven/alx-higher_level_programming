c07e4a49d534c87a38b3432275a4315a3b6b525f
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-29 16:49:16 -0700

    More python data structures

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    order_list = list(a_dictionary.keys())
+    order_list.sort()
+
+    i = 0
+    while i < len(list_ord):
+        key = order_list[i]
+        value = a_dictionary.get(key)
+        print("{}: {}".format(key, value))
+        i += 1
