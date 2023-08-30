15a32b18b3f229ca79295f63d987a97a7d065482
Mnqobi Donga <kevedonga1017@gmail.com>
2023-08-29 17:25:27 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    number = 0
+    density = 0
 
     i = 0
-    while i < len(list_keys):
-        key = list_keys[i]
-        new_dir[key] *= 2
+    while i < len(my_list):
+        number += my_list[i][0] * my_list[i][1]
+        density += my_list[i][1]
         i += 1
 
-    return (new_dir)
+    return (number / density)
