a6f4b3d884956b7215c3ddac48ef4a404f022a6e
Mnqobi Donga <kevedonga1017@gmail.com>
2023-09-19 13:36:47 -0700

    Test Driven

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def matrix_divided(matrix, div)
+def matrix_divided(matrix, div):
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
