a3ef84e26f0217cd5ba182fe7f82911eb0b52ebf
Mnqobi Donga <kevedonga1017@gmail.com>
2023-09-20 11:51:17 -0700

    Python Input and Output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""Define function."""
+"""Define class Student"""
+
 
 class Student:
+    """Represent a student"""
 
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+    def __init(self, first_name, last_name, age):
+        """
+        Initialize a new Student
 
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): First name of student
+            last_name (str): Last name of student
+            age (int): Age of student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
-        return self.__dict__
+        def to_json(self):
+            """Retrieve dictionary representation of a Student"""
+            return self.__dict__
