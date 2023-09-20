9b1cf1229adb0cf6e686d90a7ea1a38bf643623e
Mnqobi Donga <kevedonga1017@gmail.com>
2023-09-20 10:25:23 -0700

    Python Inheritance fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Define function."""
+""" Define an inherited list class MyList class"""
 
 class MyList(list):
+    """Sorted printing for the inbuilt list class"""
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
+        """Print list in ascending order"""
         print(sorted(self))
