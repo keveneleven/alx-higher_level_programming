703f22fe387e77aca34c1415495a9be5e5ad9d48
Mnqobi Donga <kevedonga1017@gmail.com>
2023-10-23 03:55:12 -0700

    Edited 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
--- A script that lists all records of table second_Table
--- Don’t list rows without a name value
--- Results should display the score and the name (in this order)
--- Records should be listed by descending score
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- select all where name is not null
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
